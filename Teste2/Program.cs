4886e03cd754b9d019e1778fdf984205cde2192a
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-05-28 17:44:04 -0300

    aprendendo o basidco kkkkk

--- Teste2/Program.cs
+++ Teste2/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string nome = "pablo";
+Console.WriteLine("meu nome é");
+Console.WriteLine(nome);
