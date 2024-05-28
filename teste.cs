e39a5048b9fafd65e24d9e7e4037df84a21d9b0e
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-05-28 18:42:53 -0300

    att

---
+++ teste.cs
@@ -0,0 +1,12 @@
+using System;
+namespace MeuAPP
+{
+    class Teste
+    {
+        static void Main(string[] args)
+        {
+            string texto = "testando";
+            Console.WriteLine(texto);
+        }
+    }
+}
