4d52c1e8ae26c2b7d01699015b4b4f1d20bf3ddc
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-05-29 20:25:02 -0300

    aprendendo variavel em c#

--- teste.cs
+++ teste.cs
@@ -5,6 +5,8 @@ namespace MeuAPP
     {
         static void Main(string[] args)
         {
+            //int idade; //correto se inicia com zero
+            int idade = 25; //correto, inicia com 25
             string texto = "testando";
             Console.WriteLine(texto);
         }
