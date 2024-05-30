3c9a1d1d8e806aec801493085509d7feb04cd961
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-05-30 19:24:58 -0300

    criando novos exemplos  e aprendendo o type system

---
+++ Teste3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Meuapp
+{
+    class Teste3
+    {
+        static void Main(string[] args)
+        {
+            string texto2 = "teste3";
+            Console.WriteLine(texto2);
+        }
+    }
+    
+}
